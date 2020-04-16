e5764a48a63cef857990145bacebc0be50dee8ef
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-17 01:33:40 +0400

    Added CarTypeService and CarTypeMapService. cloeses #4

---
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/services/CarTypeService.java
@@ -0,0 +1,7 @@
+package bahram.springframework.sfgpetclinic.services;
+
+import bahram.springframework.sfgpetclinic.model.CarType;
+
+public interface CarTypeService extends CrudService<CarType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/services/map/CarTypeMapService.java
@@ -0,0 +1,35 @@
+package bahram.springframework.sfgpetclinic.services.map;
+
+import bahram.springframework.sfgpetclinic.model.CarType;
+import bahram.springframework.sfgpetclinic.services.CarTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CarTypeMapService extends AbstractMapService<CarType, Long> implements CarTypeService {
+    @Override
+    public Set<CarType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(CarType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public CarType save(CarType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public CarType findById(Long id) {
+        return super.findById(id);
+    }
+}
