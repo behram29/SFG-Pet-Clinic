1df74279cdb801185da53c00e7e0e32bb320c3e1
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-17 18:18:15 +0400

    Added Specialty Map Service and Impl. closes #6

---
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package bahram.springframework.sfgpetclinic.services;
+
+import bahram.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
