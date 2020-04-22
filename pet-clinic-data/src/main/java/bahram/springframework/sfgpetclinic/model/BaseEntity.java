ca3da0cd781ffb4f18f5fb4e356d6a1591985ea4
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-22 17:43:29 +0400

    Implement Base Entity (JPA). closes #10

--- pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,13 @@
 package bahram.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,11 +3,16 @@ package sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
+
     }
 
 }
