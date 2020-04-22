46900255a71342a1893020b150b2aa0e3eaae0e4
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-22 22:01:11 +0400

    Updated Master to use. closes #12

--- pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/Master.java
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/Master.java
@@ -1,9 +1,17 @@
 package bahram.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="master")
 public class Master extends Person {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "master_specialties",
+            joinColumns = @JoinTable(name = "master_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities =  new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
