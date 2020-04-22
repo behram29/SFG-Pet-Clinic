5baefac5ce76635b3c33465d43179851b0f99833
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-22 20:57:59 +0400

    Updating Owner with JPA entity mapping. closes #11

--- pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/Car.java
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/Car.java
@@ -1,12 +1,24 @@
 package bahram.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="cars")
 public class Car extends BaseEntity{
 
+    @Column(name="name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name="type_id")
     private CarType carType;
+
+    @ManyToOne
+    @JoinColumn(name="owner_id")
     private Owner owner;
+
+    @Column(name="birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/CarType.java
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/model/CarType.java
@@ -1,6 +1,14 @@
 package bahram.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class CarType extends BaseEntity {
+
+    @Column(name="name")
     private String name;
 
     public String getName() {
