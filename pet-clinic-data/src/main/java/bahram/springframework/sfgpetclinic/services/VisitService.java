f441060710ddcd2cbc5564b700bcc04707bd5252
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-24 13:14:16 +0400

    Implemented Map impl for Visit . closes #21

---
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package bahram.springframework.sfgpetclinic.services;
+
+import bahram.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/bahram/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package bahram.springframework.sfgpetclinic.services.map;
+
+import bahram.springframework.sfgpetclinic.model.Visit;
+import bahram.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getCar() == null || visit.getCar().getOwner() == null || visit.getCar().getId() == null
+                || visit.getCar().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
